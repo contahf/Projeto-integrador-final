327151208899ea6d72ae76c94efde599fe60df37
contahf <conta.hf@gmail.com>
2016-10-15 18:11:20 -0300

    Alterações feita na adição das chaves estrageiras

--- projFinal/ScripdoBanco/Banco.sql
+++ projFinal/ScripdoBanco/Banco.sql
@@ -1,26 +1,26 @@
-﻿create database projetointegrador
-
+﻿--primeiro criar o banco projetointegrador
+--abra outro sql e execute o scrip abaixo
+ 
 create table curso (
 
-numero integer not null primary key,
+numero integer primary key,
 nome varchar(150) not null,
 sigla char(3) not null
-
 
 );
 
 
 create table disciplina (
 
-codigo integer not null primary key,
+codigo integer primary key,
 nome varchar(80) not null,
 ch integer not null
+
 );
 
 create table aluno(
 
-
-matricula char(15) not null primary key,
+matricula char(15) primary key,
 nome varchar(80) not null,
 sexo char(1) not null,
 dtnasc date not null,
@@ -31,19 +31,18 @@ uf  char(2) not null
 
 create table usuario (
 
-loginc varchar(20) not null primary key,
-senha varchar(80) not null,
+login varchar(20) primary key,
+senha varchar(50) not null,
 nome varchar(80)not null,
 categoria char(1)not null,
 situacao char(1) not null
-
 
 );
 
 
 create table projeto(
 
-numero serial not null primary key,
+numero serial primary key,
 ano integer not null,
 semestre integer not null,
 modulo char(3) not null,
@@ -52,60 +51,59 @@ dt_termino date not null,
 tema varchar(100) not null,
 descricao varchar(8000) not null,
 num_curso integer not null
-
 );
-
-ALTER TABLE projeto ADD CONSTRAINT fk_projeto
-FOREIGN KEY ( num_curso ) 
-REFERENCES curso( numero) ;
-
-
 
 
 create table composto(
 
-num_proj  integer not null  ,
-cod_disc integer not null  ,
+num_proj  integer,
+cod_disc integer,
 desc_atividade varchar(2000) not null,
-
- CONSTRAINT pk_CE primary key(num_proj,cod_disc)
+primary key (num_proj, cod_disc)
 );
-
-drop table composto
-
--- Foi criado a chave primaria composto devido o documento exigir duas chaves primarias
-
 
 
 create table grupo(
 
-id integer not null primary key ,
+id integer primary key,
 nome varchar(60),
 num_proj integer not null 
+
 );
 
--- Verificar essa alteração abaixo devido a tabela grupo tem chave primaria composto não conseguindo fazer o relacionamento
-ALTER TABLE grupo ADD CONSTRAINT fk_grupo 
-FOREIGN KEY ( num_proj ) 
-REFERENCES composto( num_proj) ;
 
-ALTER TABLE grupo
-DROP CONSTRAINT fk_grupo;
-
-drop table grupo
 create table participa(
 
 matricula char(15) not null,
 id_grupo integer not null ,
 nota numeric(3,1),
 
-CONSTRAINT pk_CO primary key(matricula,id_grupo)
+primary key(matricula,id_grupo)
 );
--- Foi criado a chave primaria composto devido o documento exigir duas chaves primarias
+
+
+ALTER TABLE projeto ADD CONSTRAINT fk_projeto
+	FOREIGN KEY (num_curso) 
+		REFERENCES curso(numero);
+
+--adicionar as chaves estrangeiras de acordo com a modelagem
+
+ALTER TABLE composto
+	add constraint fk_projeto foreign key (num_proj) references projeto (numero);
+
+ALTER TABLE composto
+	add constraint fk_disc foreign key (cod_disc) references disciplina (codigo);
+
+--adicionar as chaves estrangeiras de acordo com a modelagem
+ALTER TABLE grupo ADD CONSTRAINT fk_grupo 
+	FOREIGN KEY (num_proj) 
+		REFERENCES projeto(numero);
+	
+	
 ALTER TABLE participa ADD CONSTRAINT fk_participa
-FOREIGN KEY ( matricula ) 
-REFERENCES aluno( matricula ) ;
+	FOREIGN KEY ( matricula ) 
+		REFERENCES aluno( matricula );
 
 ALTER TABLE participa ADD CONSTRAINT fk_grupo
-FOREIGN KEY ( id_grupo ) 
-REFERENCES grupo( id ) ;
+	FOREIGN KEY ( id_grupo ) 
+		REFERENCES grupo( id );
