57cb89dfa49a9bc4c17fe70741693d29a5833c04
adeltonhenrique <adelton.henrique@gmail.com>
2016-10-29 21:35:13 -0200

    Evitando sql Injection

--- projFinal/painel/pages/info.php
+++ projFinal/painel/pages/info.php
@@ -2,39 +2,47 @@
 <?php
  session_start();
 	
-	$login = $_POST['login'];
-	$senha = $_POST['senha'];
 
+
+    //$login = $_POST['login'];
+    //$senha = $_POST['senha'];
+    //abaixo evitando sql_injection
+     $login = preg_replace('/[^[:alpha:]_]/', '',$_POST['login']);
+     $senha = preg_replace('/[^[:alnum:]_]/', '',$_POST['senha']);
+    
 	$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
 
 	$con = pg_connect($strCon);
 	
 
 	if ($con) {
+      
+echo "$login";
+echo "$senha";
         $user = "select * from usuario where login = '".$login."' and senha = '".$senha."'";
         $userSim = pg_query($con,$user);
 
         if(pg_num_rows($userSim) > 0 ){
             
 
-            $arrayLista = pg_fetch_array($userSim);
-            $login = $arrayLista['login'];
-            $senha = $arrayLista['senha'];
-            $nome = $arrayLista['nome'];
-            $categoria = $arrayLista['categoria'];
-            $situacao = $arrayLista['situacao'];
+           $arrayLista = pg_fetch_array($userSim);
+           $login = $arrayLista['login'];
+           $senha = $arrayLista['senha'];
+           $nome = $arrayLista['nome'];
+           $categoria = $arrayLista['categoria'];
+           $situacao = $arrayLista['situacao'];
 
-            $_SESSION['login'] = $login;
-			$_SESSION['senha'] = $senha;
-            $_SESSION['tipo']  = $categoria;
-            $_SESSION['nome']  = $nome;
-            $_SESSION['sit']  = $situacao;
+           $_SESSION['login'] = $login;
+		   $_SESSION['senha'] = $senha;
+           $_SESSION['tipo']  = $categoria;
+           $_SESSION['nome']  = $nome;
+           $_SESSION['sit']  = $situacao;
 
             
             
       
                 header ("location:/Projeto-integrador-final/projFinal/painel/pages/index.php");
-               //include 'teste.php';
+               include 'teste.php';
          
          }else{
          	
