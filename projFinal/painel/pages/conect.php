7e23c053b4de57e2d2c201c1c1f87931296ef8c7
adeltonhenrique <adelton.henrique@gmail.com>
2016-10-29 20:19:04 -0200

    Evitando Sql_injection

--- projFinal/painel/pages/conect.php
+++ projFinal/painel/pages/conect.php
@@ -1,8 +1,13 @@
 
 <?php
 	session_start();
-	$login = $_POST['login'];
-	$senha = $_POST['senha'];
+	//$login = $_POST['login'];
+	//$senha = $_POST['senha'];
+	//abaixo evitando sql_injection
+	$login = preg_replace('/[^[:alpha:]_]/', '',$_POST['login']);
+	$senha = preg_replace('/[^[:alpha:]_]/', '',$_POST['senha']);
+
+
 	$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
 
 	$con = pg_connect($strCon);
