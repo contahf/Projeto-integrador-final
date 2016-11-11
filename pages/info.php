626e2e7312247ab3b0858f6edd09ce9978c8a1d3
Henrique Ferreira <conta.hf@gmail.com>
2016-11-11 17:14:20 -0200

    include 'conect.php'

--- pages/info.php
+++ pages/info.php
@@ -4,9 +4,7 @@
 session_start();
 
 try {
-            
-        $strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
-        $con = pg_connect($strCon);
+        include 'conect.php';
 
         if(!$con){
                     
