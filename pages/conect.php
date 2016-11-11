6db53ad16384983df0f4d6919a558392810bc144
Henrique Ferreira <conta.hf@gmail.com>
2016-11-11 17:11:25 -0200

    Conexão com o banco de dados

--- pages/conect.php
+++ pages/conect.php
@@ -1,22 +1,7 @@
-
-<?php
-	session_start();
-	//$login = $_POST['login'];
-	//$senha = $_POST['senha'];
-	//abaixo evitando sql_injection
-	$login = preg_replace('/[^[:alpha:]_]/', '',$_POST['login']);
-	$senha = preg_replace('/[^[:alpha:]_]/', '',$_POST['senha']);
-
-
-	$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
-
-	$con = pg_connect($strCon);
+<?php 
 	
-
-	if ($con) {
-        
-        return;       	   
-
-    }
+	$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
+	$con = pg_connect($strCon);
 
 ?>
+
