92678506b0a92959223644897cd403fe2bf4088a
Henrique Ferreira <conta.hf@gmail.com>
2016-11-16 19:15:43 -0200

    try catch

--- pages/conect.php
+++ pages/conect.php
@@ -1,7 +1,16 @@
 <?php 
 	
-	$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
-	$con = pg_connect($strCon);
+	try {
+		
+		$strCon = "host=localhost dbname=projetointegrador port=5432 user=senac password=senac123";
+		$con = pg_connect($strCon);	
+
+	} catch (Exception $e) {
+		
+		throw new Exception("Verifique a conexão com seu banco", 1);
+
+		echo $e->getMessage(), "\n";
+	}
 
 ?>
 
