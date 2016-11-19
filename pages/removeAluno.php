8bd6b8460ecad2903d05faf109c4f0cab66bdac4
Henrique Ferreira <conta.hf@gmail.com>
2016-11-19 15:10:04 -0200

    Melhoria no codigo de remoção de aluno

--- pages/removeAluno.php
+++ pages/removeAluno.php
@@ -1,4 +1,5 @@
  <?php  
+   
    session_start();
 
     if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true)) 
@@ -8,211 +9,48 @@
         session_destroy();
         header('location:../index.html');
     }
-    
-    $mat = $_POST['txtMat']; 
-    
-    $strCon = "host=localhost dbname=projetointegrador user=senac password=senac123";
-    $con = pg_connect($strCon);
 
-    if($con){
     
-        $sql = "select * from aluno where matricula = '". $mat ."'";
-        $result = pg_query($con, $sql);
+    $mat = addslashes($_GET['txtMat']);
+
+    if (!isset($mat) && empty($mat)) {
+    
+         echo (json_encode("-1"));
+    
+     }else{
+
+        include 'conect.php';
+        
+        if($con){
+    
+            $sql = "SELECT * from aluno where matricula = '". $mat ."'";
+            $result = pg_query($con, $sql);
        
 
-        if(pg_affected_rows($result) > 0){
-            $sql = "";
-            $sql = "DELETE FROM aluno WHERE matricula = " . "'" . $mat ."'";
-            $result = "";
-            $result = pg_query($con, $sql);
-
-                if(pg_affected_rows($result) > 0){
+            if(pg_num_rows($result) > 0){
         
-                      echo "<script type='text/javascript'>
-                                                                        
-                         window.alert('Aluno removido com sucesso!');
-                       window.location.href = 'removeAluno.php'; 
-                
-                         </script>";
-                }
+                $sql = "DELETE FROM aluno WHERE matricula = '" . $mat ."'";
+        
+                $result = pg_query($con, $sql);
+
+                    if(pg_affected_rows($result) > 0){
+                    
+                        echo (trim($mat));
+                    
+                    }else{
+                        
+                        return false;
+                    }
+            }else{
+
+                echo "Registro não encontrado";
+            }
         }
-    }
 
-//pg_close($con);
 
+    } 
+    
+    
+    pg_close($con);
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>TI Resolve</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                
-                <a class="navbar-brand" href="#">T.I Resolve</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-               
-                    
-                    <li>
-                        <a href="out.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                   
-                 <ul class="nav" id="side-menu">
-                      
-                        <li>
-                            <a href="index.php"><i class="fa fa-home fa-fw"></i> Home</a>
-                        </li>
-                          
-                          <li>
-                            <a href="#"><i class="fa fa-user fa-fw"></i> Aluno<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="cadAluno.php">Incluir </a>
-                                </li>
-								<li>
-                                    <a href="confMatricula.php">Alterar </a>
-                                </li>
-								<li>
-                                    <a href="confMatricula.php">Consultar </a>
-                                </li>
-							
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-
-                         
-                       
-
-                        <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Informações<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="removeDis.php">Sobre o projeto</a>
-                                </li>
-                                <li>
-                                    <a href="dis.php">Autores</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        
-                        
-                    </ul>
-
-
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-       
-
-        <div id="page-wrapper">
-           
-            <br>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Remove aluno(a)
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="">
-                                    <form action="" method="post">
-                                        
-                                        <div class="form-group col-lg-3" >
-                                            <label>Informe a matricula do aluno</label>
-                                            <input type="text" class="form-control" name="txtMat" required="">
-                                            
-                                        </div>
-
-
-                                         <div class="clearfix"></div>
-                                         
-                                         <div class="container">
-                                            <button type="submit" class="btn btn-success">Confirmar</button>
-                                            <button type="reset" class="btn btn-default">Cancelar</button>
-                                    
-                                         </div>
-                                                                            
-                                    </form>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                           
-                                <!-- /.col-lg-6 (nested) -->
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
-</body>
-
-</html>
