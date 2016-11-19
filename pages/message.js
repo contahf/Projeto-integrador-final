ff498c5ead0b60623ba4d8e9bcfc00799a6ae243
Henrique Ferreira <conta.hf@gmail.com>
2016-11-19 15:04:42 -0200

    Exclusão de paginas

--- pages/message.js
+++
@@ -1,9 +0,0 @@
- <script type='text/javascript'>
-                                                                        
-
-             window.alert('Ja cadastrado!');
-             window.location.href = 'index.php'; 
-
-                                                                        
-                        
- </script>
--- pages/sucesso.js
+++
@@ -1,9 +0,0 @@
- <script type='text/javascript'>
-                                                                        
-
-             window.alert('Cadastrado realizado!');
-             window.location.href = 'index.php'; 
-
-                                                                        
-                        
- </script>
