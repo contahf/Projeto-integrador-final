cbfe3b6c1954eee5cb26aca056165332bd97a65f
hfs <hfs@0.0.12.228>
2016-11-21 00:09:34 -0200

    Script cadastro de aluno

--- pages/aluno.php
+++ pages/aluno.php
@@ -10,53 +10,91 @@
         header('location:../index.html');
     }
  
-    $ret = "";
+    $nome = addslashes($_POST['txtNome']);
+    $matricula = addslashes($_POST['txtMat']);
+    $nasc = addslashes($_POST['txtNasc']);
+    $sex = addslashes($_POST['txtSexo']);
+    $cid = addslashes($_POST['txtCidade']);
+    $u = addslashes($_POST['estado']);
 
-    $nome = $_POST['txtNome'];
-    $matricula = $_POST['txtMat'];
-    $nasc = $_POST['txtNasc'];
-    $sex = $_POST['txtSexo'];
-    $cid = $_POST['txtCidade'];
-    $u = $_POST['estado'];
-    
 
-    require_once 'conect.php';
+    $errors = array();
+    $data = array();
 
-    if ($con) {
-        
-        $sql = "select * from aluno where  matricula = '". $matricula ."'";
-
-        $consulta = pg_query($con, $sql);
- 
-        $linhas = pg_num_rows($consulta);
-
-        if($linhas > 0 ){
-            
-            $ret = "-1";
-            
-         }elseif ($linhas==0) {
-           
-            $sql = "INSERT INTO aluno (matricula, nome, sexo, dtnasc, cidade, uf) VALUES ('".$matricula."', '".$nome."', '".$sex."','".$nasc."','".$cid."', '".$u."');";
-            $res = pg_query($con, $sql);
-
-            $qtd_linhas = pg_affected_rows($res);
-
-            if ($qtd_linhas > 0){
-                
-                $ret = $matricula;
-                
-            }
-
-        
-         }
-
-                
-
+    if (empty($nome)){
+        $errors['n'] = '10';
     }
 
-    echo (json_encode($ret));
+    if (empty($matricula)){
+        
+        $errors['mat'] = '11';
+    }
+
+    if (empty($nasc)){
+
+        $errors['nas'] = '12';
+    }
+
+    if (empty($sex)){
+
+        $errors['s'] = '13';
+    }
+    if (empty($cid)){
+
+        $errors['cid'] = '14';
+    }
+
+    if (empty($u)){
+
+        $errors['est'] = '15';
+    }
+
+    if (!empty($errors)) {
+    
+        $b = $errors;
+        
+    }else{
+
+        require_once 'conect.php';
+
+        if ($con) {
+            
+            $sql = "select * from aluno where  matricula = '". $matricula ."'";
+
+            $consulta = pg_query($con, $sql);
+     
+            $linhas = pg_num_rows($consulta);
+
+            if($linhas > 0 ){
+                
+                $errors['existe'] = "-1";
+                $b = $errors;
+                
+             }elseif ($linhas==0) {
+               
+                $sql = "INSERT INTO aluno (matricula, nome, sexo, dtnasc, cidade, uf) VALUES ('".$matricula."', '".$nome."', '".$sex."','".$nasc."','".$cid."', '".$u."');";
+                $res = pg_query($con, $sql);
+
+                $qtd_linhas = pg_affected_rows($res);
+
+                if ($qtd_linhas > 0){
+                    
+                    $data['success'] = $matricula;
+                    $b = $data;                
+                }
+
+            
+             }
+
+                    
+
+        }
+
+
+    }
+    
+    echo (json_encode($b));
+
     pg_close($con);
     
-
-
 ?>
