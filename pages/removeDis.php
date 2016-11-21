681924acc1ad50799b01f4e3877b8a45171d65a9
Henrique Ferreira <conta.hf@gmail.com>
2016-11-21 21:19:33 -0200

    Script remoção de disciplina

--- pages/removeDis.php
+++ pages/removeDis.php
@@ -1,4 +1,5 @@
  <?php  
+  
    session_start();
 
     if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true)) 
@@ -9,185 +10,40 @@
         header('location:../index.html');
     }
     
-    $codigo = $_GET['codDis']; 
+    $data;
+    $arr = "";
     
-    $strCon = "host=localhost dbname=projetointegrador user=senac password=senac123";
-    $con = pg_connect($strCon);
-
-    if($con){
+    $codigo = $_GET['numDis']; 
+    
+    require_once 'conect.php';
     
         $sql = "select * from disciplina where codigo = '". $codigo ."'";
         $result = pg_query($con, $sql);
        
-
         if(pg_affected_rows($result) > 0){
-            $sql = "";
+
             $sql = "DELETE FROM disciplina WHERE codigo = " . "'" . $codigo ."'";
-            $result = "";
-            $result = pg_query($con, $sql);
+            $result = pg_query($sql);
 
                 if(pg_affected_rows($result) > 0){
         
-                      echo "<script type='text/javascript'>
-                                                                        
-                         window.alert('Disciplina removida com sucesso!');
-                       window.location.href = 'editarDis.php'; 
-                
-                         </script>";
+                    $data = trim($codigo);
+
+                }else{
+
+                    $data = "-1"; 
                 }
+        }else{
+
+            $data = "-2";
+
         }
-    }
+
+        echo $data;
+    
+    
 
 pg_close($con);
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>TI Resolve</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style type="text/css">
-        #div {
- 
-       
-        position:absolute; 
-        
-        margin-top:-100px; /* ou seja ele pega 50% da altura tela e sobe metade do valor da altura no caso 100 */
-        left:50%;
-        margin-left:-250px; /* ou seja ele pega 50% da largura tela e diminui  metade do valor da largura no caso 250 */
-        
- 
-        }
-    </style>
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-       <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                
-                <a class="navbar-brand" href="index.html">T.I Resolve</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-               
-                    
-                    <li>
-                        <a href="out.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                   
-                 <?php   include "principal.html"; ?>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-
-        <div id="page-wrapper" >
-           
-            <br>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default" >
-                        <div class="panel-heading">
-                            Remoção da disciplina
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="" >
-                                    <form action="" method="post">
-                                        <div >
-                                            <div class="form-group col-lg-3" >
-                                                <label>Confirme o codigo da disciplina</label>
-                                                <input type="numeric" class="form-control" name="txtCod" required="">
-                                                
-                                            </div>
-
-                                        </div>
-                                       
-
-
-                                         <div class="clearfix"></div>
-                                         
-                                         <div class="container">
-                                            <button type="submit" class="btn btn-success">Confirmar</button>
-                                            <button type="reset" class="btn btn-default">Cancelar</button>
-                                    
-                                         </div>
-
-                                        
-
-                                                                            
-                                    </form>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                           
-                                <!-- /.col-lg-6 (nested) -->
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
-</body>
-
-</html>
